04551bb4cfd04aaa6c32a4ed69fd8acd5629d92e
Otim0310 <rkdmsdlaa123@naver.com>
2025-06-03 00:48:38 +0900

    Update MemberController.java

--- geaun/likion-crud/src/main/java/com/likelion/likelioncrud/member/api/MemberController.java
+++ geaun/likion-crud/src/main/java/com/likelion/likelioncrud/member/api/MemberController.java
@@ -12,7 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-// http://localhost:8080/member/save
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/members")
