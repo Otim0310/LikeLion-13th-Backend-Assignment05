8d8e261e9c9059d9043d460a1b00907c5d989e24
Otim0310 <rkdmsdlaa123@naver.com>
2025-06-03 01:01:54 +0900

    Update PostController.java

--- geaun/likion-crud/src/main/java/com/likelion/likelioncrud/post/api/PostController.java
+++ geaun/likion-crud/src/main/java/com/likelion/likelioncrud/post/api/PostController.java
@@ -1,12 +1,13 @@
 package com.likelion.likelioncrud.post.api;
 
+import com.likelion.likelioncrud.common.template.ApiResTemplate;
+import com.likelion.likelioncrud.common.error.SuccessCode;
 import com.likelion.likelioncrud.post.api.dto.request.PostSaveRequestDto;
 import com.likelion.likelioncrud.post.api.dto.request.PostUpdateRequestDto;
 import com.likelion.likelioncrud.post.api.dto.response.PostListResponseDto;
 import com.likelion.likelioncrud.post.application.PostService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,35 +17,29 @@ import org.springframework.web.bind.annotation.*;
 public class PostController {
     private final PostService postService;
 
-    // 게시물 저장
-    @PostMapping("/save")
-    public ResponseEntity<String> postSave(@RequestBody @Valid PostSaveRequestDto postSaveRequestDto) {
+    @PostMapping
+    public ResponseEntity<ApiResTemplate<Void>> postSave(@RequestBody @Valid PostSaveRequestDto postSaveRequestDto) {
         postService.postSave(postSaveRequestDto);
-        return new ResponseEntity<>("게시물 저장!", HttpStatus.CREATED);
+        return ResponseEntity.ok(ApiResTemplate.successWithNoContent(SuccessCode.POST_SAVE_SUCCESS));
     }
 
-    // 사용자 id를 기준으로 해당 사용자가 작성한 게시글 목록 조회
     @GetMapping("/{memberId}")
-    public ResponseEntity<PostListResponseDto> myPostFindAll(@PathVariable("memberId") Long memberId) {
+    public ResponseEntity<ApiResTemplate<PostListResponseDto>> myPostFindAll(@PathVariable("memberId") Long memberId) {
         PostListResponseDto postListResponseDto = postService.postFindMember(memberId);
-        return new ResponseEntity<>(postListResponseDto, HttpStatus.OK);
+        return ResponseEntity.ok(ApiResTemplate.successResponse(SuccessCode.GET_SUCCESS, postListResponseDto));
     }
 
-    //게시물 id를 기준으로 사용자가 작성한 게시물 수정
     @PatchMapping("/{postId}")
-    public ResponseEntity<String> postUpdate(
+    public ResponseEntity<ApiResTemplate<Void>> postUpdate(
             @PathVariable("postId") Long postId,
             @RequestBody PostUpdateRequestDto postUpdateRequestDto) {
         postService.postUpdate(postId, postUpdateRequestDto);
-        return new ResponseEntity<>("게시물 수정", HttpStatus.OK);
+        return ResponseEntity.ok(ApiResTemplate.successWithNoContent(SuccessCode.POST_UPDATE_SUCCESS));
     }
 
-    //게시물 id를 기준으로 사용자가 작성한 게시물 삭제
     @DeleteMapping("/{postId}")
-    public ResponseEntity<String> postDelete(
-            @PathVariable("postId") Long postId) {
+    public ResponseEntity<ApiResTemplate<Void>> postDelete(@PathVariable("postId") Long postId) {
         postService.postDelete(postId);
-        return new ResponseEntity<>("게시물 삭제", HttpStatus.OK);
-
+        return ResponseEntity.ok(ApiResTemplate.successWithNoContent(SuccessCode.POST_DELETE_SUCCESS));
     }
 }
